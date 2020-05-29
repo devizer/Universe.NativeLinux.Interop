762ec0d27672da51e405ade148515a4044abf34b
devizer <devizer@github.io>
2020-05-30 02:10:07 +0300

    azure-steps-nix.yml: publish nupkg

--- Universe.LinuxTaskstat/Universe.LinuxTaskstat/build-nuget.sh
+++ Universe.LinuxTaskstat/Universe.LinuxTaskstat/build-nuget.sh
@@ -2,16 +2,17 @@
 pushd ../../version
 source build-version.sh;
 popd
-Say "VERSION to build: $Version"
+Say "VERSION to build: [$Version]. Building"
 
 rm -rf bin/Release || true
 dotnet build -c Release /p:"PackageVersion=$Version" /p:"Version=$Version"
 monodis --assembly bin/Release/net40/Universe.LinuxTaskstat.dll
 cp -f Universe.LinuxTaskstat.nuspec bin/Release
-exit;
+
+Say "VERSION to build: [$Version]. Removing auto packaged package"
 pushd bin/Release
-mkdir -p .prev
-mv -f *.nupkg .prev
+7z a -sdel nupkgs-prev-packed.7z *nupkg || true
+Say "VERSION to build: [$Version]. Nu-Packaging"
 nuget pack Universe.LinuxTaskstat.nuspec -Version "$Version"
 popd
 
