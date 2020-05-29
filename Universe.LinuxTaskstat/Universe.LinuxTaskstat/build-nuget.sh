f92264443aa4c0f71e200a1a74810aef738cb6f6
devizer <devizer@github.io>
2020-05-30 01:51:45 +0300

    build-nuget.sh: versioning

--- Universe.LinuxTaskstat/Universe.LinuxTaskstat/build-nuget.sh
+++ Universe.LinuxTaskstat/Universe.LinuxTaskstat/build-nuget.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
+pushd ../../version
+source build-version.sh;
+popd
+Say "VERSION to build: $Version"
+
 rm -rf bin/Release || true
-dotnet build -c Release
+dotnet build -c Release /p:"PackageVersion=$Version" /p:"Version=$Version"
+monodis --assembly bin/Release/net40/Universe.LinuxTaskstat.dll
 cp -f Universe.LinuxTaskstat.nuspec bin/Release
+exit;
 pushd bin/Release
 mkdir -p .prev
 mv -f *.nupkg .prev
-nuget pack Universe.LinuxTaskstat.nuspec
+nuget pack Universe.LinuxTaskstat.nuspec -Version "$Version"
 popd
 
