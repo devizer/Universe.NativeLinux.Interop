f98d51a823263be2d15714d65a321b140bc3c7ab
devizer <devizer@github.io>
2020-05-31 01:06:23 +0300

    azure-pipelines.yml: job name, local show-taskstats-structure.sh

--- so-src/re-build-local.sh
+++ so-src/re-build-local.sh
@@ -2,3 +2,7 @@
 gcc -O2 -shared -fPIC -o libNativeLinuxInterop.so gettaskstat.c
 gcc -O2 -o /tmp/show-taskstat-info show-taskstat-info.c
 /tmp/show-taskstat-info
+
+pushd taskstats-internals >/dev/null
+bash show-taskstats-structure.sh
+popd >/dev/null
