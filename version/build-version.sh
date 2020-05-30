478e786d42e1552a52c31bc30b82fd47a095e27b
devizer <devizer@github.io>
2020-05-30 04:45:56 +0300

    version and description 3rd

--- version/build-version.sh
+++ version/build-version.sh
@@ -2,7 +2,7 @@
 version_build=$(set TZ=GMT; git log -n 999999 --date=raw --pretty=format:"%cd" | wc -l)
 version_main=$(cat version-main)
 # on the build server we use it to find the nupkg artifact
-tf_build=${BUILD_BUILDID}; if [[ $tf_build -gt 0 ]]; then tf_build=".${tf_build}"  
+tf_build=${BUILD_BUILDID}; if [[ $tf_build -gt 0 ]]; then tf_build=".${tf_build}"; fi  
 # version_suffix=$(cat version-suffix)
 export Version="${version_main}.${version_build}${tf_build}${version_suffix}"
 
